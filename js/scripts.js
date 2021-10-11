b7e6934c4423d900d1b32c85884384f8f6d1b145
Marilzon <maxmaril@hotmail.com>
2021-10-10 23:05:26 -0300

    Feature: Arrays manipulations with for condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+var monthTemperature = [];
+
+monthTemperature[0] = 31.9;
+monthTemperature[1] = 35.6;
+monthTemperature[2] = 42;
+monthTemperature[3] = 38;
+monthTemperature[4] = 25.5;
+
+for (key in monthTemperature) {
+  console.log(`${monthTemperature[key]}ºC`);
+}
